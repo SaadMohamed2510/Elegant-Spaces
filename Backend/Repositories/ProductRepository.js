ea087bf09777e492eaca124f9581b174ae4bda15
rawan-elfiyady <207576146+SaadMohamed2510@users.noreply.github.com>
2025-06-16 19:53:19 +0300

    Update ProductRepository.js

--- Backend/Repositories/ProductRepository.js
+++ Backend/Repositories/ProductRepository.js
@@ -1,1 +1,16 @@
 const supabase = require("../Config/SupabaseClient");
+
+async function CreateProduct(product) {
+    const{data, error} = await supabase
+    .from("Products")
+    .insert([product])
+    .single();
+    if(error) throw new error(error.message);
+
+    return data;
+}
+
+module.exports = {
+    CreateProduct
+
+};
